9a70e4ae79dde9f5b8dcc357737218485e8c0a48
ajaykumar2004 <ajaykumar30802004@gmail.com>
2023-12-15 10:38:25 +0530

    Demo for Team

---
+++ src/main/java/com/mavenweb/PresentMaven/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.PresentMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
